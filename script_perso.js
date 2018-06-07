227c4fde2cf3ab076bd7de1101b701a4d4cedc65
Marc Banières <marc.banieres@gmail.com>
2018-06-07 10:53:58 +0200

    first commit

---
+++ script_perso.js
@@ -0,0 +1,73 @@
+// alert('perso_script.js');
+// document.getElementById('h2').textContent += 'Modifié en javscript';
+
+// var newtext = 'qelque chose';
+// var newtext = document.createTextNode('qelque chose');
+// document.getElementById('h2').appendChild(newtext);
+
+// Maj compteur Message
+
+function supprime_ligne( ligne ){
+
+  var nbr_ligne = document.getElementsByClassName('ligne').length;
+
+  if( nbr_ligne == 1 ){
+    if (window.confirm("Voulez-vous réellement tout supprimer ?")) {
+        ligne.remove();
+    }
+
+  } else {
+    ligne.remove();
+  }
+
+  maj_compteur();
+
+}
+
+
+
+maj_compteur();
+
+function maj_compteur(){
+  var nbr_ligne = document.getElementsByClassName('ligne').length;
+  document.getElementById('count').textContent = nbr_ligne;
+  if( nbr_ligne == 0 ){
+    document.getElementById("count").className += " alert";
+  }
+}
+
+
+
+
+
+
+for(var i=0; i<document.getElementsByClassName('ligne').length; i++) {
+
+        // console.log('Ligne : '+i);
+        document.getElementsByClassName('ligne')[i].order = i+1;
+
+        // Pour le fun -> affiche le numéro initial de la ligne
+        document.getElementsByClassName('ligne')[i].addEventListener("click",
+          function(){
+           console.log('Ligne : '+ this.order);
+          }
+        );
+
+}
+
+
+
+
+
+
+for(var i=0; i<document.getElementsByClassName('poubelle').length; i++) {
+
+        // Exo
+        document.getElementsByClassName('poubelle')[i].addEventListener("click",
+          function(){
+            // this.parentElement.remove();
+            supprime_ligne( this.parentElement );
+          }
+        );
+
+}
